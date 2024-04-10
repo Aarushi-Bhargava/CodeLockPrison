9c8d6e5ab4667ec70858e68a7aec9282d008294c
Aarushi Bhargava <bhargava.aarushi2006@gmail.com>
2024-04-10 13:22:37 -0400

    Updated game name

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import arcade
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Sprites and Periodic Bullets Example"
+SCREEN_TITLE = "Shooting Game Scratch Code Version"
 
 
 class EnemySprite(arcade.Sprite):
