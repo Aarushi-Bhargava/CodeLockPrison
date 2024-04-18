5efd991e0796aa5aac090759d1eb57b2974296dd
voconaco <celine11306@gmail.com>
2024-04-17 20:23:25 -0400

    Shooting Game (Randomly Moving Projectiles & Moving User's Avatar)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ If Python and Arcade are installed, this example can be run from the command lin
 python -m arcade.examples.sprite_bullets_periodic
 """
 import arcade
+import random
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
@@ -12,41 +13,51 @@ SCREEN_TITLE = "Shooting Game Renamed"
 
 PLAYER_MOVEMENT_SPEED = 5
 
-class EnemySprite(arcade.Sprite):
-    """ Enemy ship class that tracks how long it has been since firing. """
 
-    def __init__(self, image_file, scale, bullet_list, time_between_firing):
+class EnemySprite(arcade.Sprite):
+    """ Enemy ship class that tracks how long it has been since firing and moves left and right.
+    It fires at random intervals. """
+
+    def __init__(self, image_file, scale, bullet_list, min_time_between_firing, max_time_between_firing, movement_speed,
+                 boundary_left, boundary_right):
         """ Set up the enemy """
         super().__init__(image_file, scale)
-
-        # How long has it been since we last fired?
         self.time_since_last_firing = 0.0
-
-        # How often do we fire?
-        self.time_between_firing = time_between_firing
-
-        # When we fire, what list tracks the bullets?
+        self.min_time_between_firing = min_time_between_firing
+        self.max_time_between_firing = max_time_between_firing
+        self.time_between_firing = random.uniform(min_time_between_firing, max_time_between_firing)
         self.bullet_list = bullet_list
+        self.movement_speed = movement_speed
+        self.boundary_left = boundary_left
+        self.boundary_right = boundary_right
+        self.movement_direction = 1  # 1 for right, -1 for left
 
     def on_update(self, delta_time: float = 1 / 60):
         """ Update this sprite. """
+        # Update position
+        self.center_x += self.movement_speed * self.movement_direction
+
+        # Check if we hit a boundary and need to reverse
+        if self.right >= self.boundary_right or self.left <= self.boundary_left:
+            self.movement_direction *= -1
 
         # Track time since we last fired
         self.time_since_last_firing += delta_time
 
         # If we are past the firing time, then fire
         if self.time_since_last_firing >= self.time_between_firing:
+            self.fire_bullet()
 
-            # Reset timer
-            self.time_since_last_firing = 0
-
-            # Fire the bullet
-            bullet = arcade.Sprite("CodeLockPrison/768px-Eo_circle_red_blank.svg.png", scale=0.05)
-            bullet.center_x = self.center_x
-            bullet.angle = -90
-            bullet.top = self.bottom
-            bullet.change_y = -2
-            self.bullet_list.append(bullet)
+    def fire_bullet(self):
+        """ Fire a bullet and reset the firing timer to a random interval. """
+        self.time_since_last_firing = 0
+        self.time_between_firing = random.uniform(self.min_time_between_firing, self.max_time_between_firing)
+        bullet = arcade.Sprite("CodeLockPrison/768px-Eo_circle_red_blank.svg.png", scale=0.04)
+        bullet.center_x = self.center_x
+        bullet.angle = -90
+        bullet.top = self.bottom
+        bullet.change_y = -2
+        self.bullet_list.append(bullet)
 
 
 class MyGame(arcade.Window):
@@ -75,29 +86,31 @@ class MyGame(arcade.Window):
         self.wall_list = arcade.SpriteList()
 
         # Add player ship
-        self.player = arcade.Sprite("CodeLockPrison/images__2_-removebg-preview.png", scale=0.5)
+        self.player = arcade.Sprite("CodeLockPrison/images__2_-removebg-preview.png", scale=0.3)
+        self.player.center_x = SCREEN_WIDTH / 2
+        self.player.center_y = 50
         self.player_list.append(self.player)
 
         # Add top-left enemy ship
         enemy = EnemySprite("CodeLockPrison/600px-Piste_Scandinavia_3_red_rectangle.svg.png",
                             scale=0.1,
                             bullet_list=self.bullet_list,
-                            time_between_firing=1.0)
-        enemy.center_x = SCREEN_WIDTH/2
+                            min_time_between_firing=0.5,  # Minimum time between shots
+                            max_time_between_firing=2.0,  # Maximum time between shots
+                            movement_speed=2,
+                            boundary_left=50,
+                            boundary_right=SCREEN_WIDTH - 50)
+        enemy.center_x = SCREEN_WIDTH / 2
         enemy.center_y = SCREEN_HEIGHT - enemy.height
         enemy.angle = 180
         self.enemy_list.append(enemy)
 
-        # Add a crate on the ground
-        wall = arcade.Sprite(
-            "CodeLockPrison/600px-Piste_Scandinavia_3_red_rectangle.svg.png", 1
-        )
-        wall.position = [-100,-500]
+        # Add a crate on the ground (not visible, example placeholder)
+        wall = arcade.Sprite("CodeLockPrison/600px-Piste_Scandinavia_3_red_rectangle.svg.png", scale=1)
+        wall.position = [-100, -500]
         self.wall_list.append(wall)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(
-            self.player, self.wall_list
-        )
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.wall_list)
 
     def on_draw(self):
         """Render the screen. """
@@ -111,33 +124,23 @@ class MyGame(arcade.Window):
     def on_update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
 
-        # Call on_update for each enemy in  the list
         self.enemy_list.on_update(delta_time)
 
         # Get rid of the bullet when it flies off-screen
         for bullet in self.bullet_list:
             if bullet.top < 0:
                 bullet.remove_from_sprite_lists()
-        
+
         self.bullet_list.update()
+        self.player_list.update()
 
-        """Movement and game logic"""
-
-        # Keep player in the center 
-        if self.player.center_x < 0:
-            self.player.center_x = 0
-        elif self.player.center_x > SCREEN_WIDTH:
-            self.player.center_x = SCREEN_WIDTH
+        # Constrain the player to stay within the screen boundaries
+        if self.player.left < 0:
+            self.player.left = 0
+        if self.player.right > SCREEN_WIDTH:
+            self.player.right = SCREEN_WIDTH
 
         self.physics_engine.update()
-
-
-    # def on_mouse_motion(self, x, y, delta_x, delta_y):
-    #     """
-    #     Called whenever the mouse moves.
-    #     """
-    #     self.player.center_x = x
-    #     self.player.center_y = 20
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
@@ -150,9 +153,7 @@ class MyGame(arcade.Window):
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key."""
 
-        if key == arcade.key.LEFT:
-            self.player.change_x = 0
-        elif key == arcade.key.RIGHT:
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.player.change_x = 0
 
 
